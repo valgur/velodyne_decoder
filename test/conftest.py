b27825cb47f1312145cd253bb632b29d694af1f2
Martin Valgur <martin.valgur@gmail.com>
2021-11-11 14:38:37 +0200

    add unit tests

---
+++ test/conftest.py
@@ -0,0 +1,28 @@
+import tempfile
+from pathlib import Path
+
+import pytest
+import requests
+
+data_dir = Path(tempfile.tempdir) / "velodyne_decoder_data"
+base_url = "https://github.com/valgur/velodyne_decoder/releases/download/v1.0.1/"
+
+
+def fetch(name):
+    url = base_url + name
+    if not data_dir.exists():
+        data_dir.mkdir()
+    path = data_dir / name
+    if not path.exists():
+        path.write_bytes(requests.get(url).content)
+    return path
+
+
+@pytest.fixture
+def sample_pcap_path():
+    return fetch("vlp16.pcap")
+
+
+@pytest.fixture
+def sample_bag_path():
+    return fetch("vlp16.bag")
---
+++ test/test_velodyne_decoder.py
@@ -0,0 +1,71 @@
+import pytest
+
+import velodyne_decoder as vd
+
+pcl_struct_dtype = {'names': ['x', 'y', 'z', 'intensity', 'ring', 'time'],
+                    'formats': ['<f4', '<f4', '<f4', '<f4', '<u2', '<f4'],
+                    'offsets': [0, 4, 8, 16, 20, 24], 'itemsize': 32}
+
+
+@pytest.fixture
+def config():
+    config = vd.Config()
+    config.model = "VLP-16"
+    config.rpm = 600
+    return config
+
+
+def test_pcap_as_contiguous_array(sample_pcap_path, config):
+    pcds = list(vd.read_pcap(sample_pcap_path, config, as_pcl_structs=False))
+    assert len(pcds) == 93
+    stamp, pcd = pcds[0]
+    assert stamp == 1427759049.258258
+    assert pcd.shape == (27282, 6)
+    assert pcd.dtype.name == "float32"
+
+
+def test_pcap_as_struct_array(sample_pcap_path, config):
+    pcds = list(vd.read_pcap(sample_pcap_path, config, as_pcl_structs=True))
+    assert len(pcds) == 93
+    stamp, pcd = pcds[0]
+    assert stamp == 1427759049.258258
+    assert pcd.shape == (27282,)
+    assert pcd.dtype == pcl_struct_dtype
+
+
+def test_bag_as_contiguous_array(sample_bag_path, config):
+    pcds = list(vd.read_bag(sample_bag_path, config, "/velodyne_packets", as_pcl_structs=False))
+    assert len(pcds) == 13
+    stamp, pcd, topic = pcds[0]
+    assert topic == "/velodyne_packets"
+    assert stamp.to_sec() == 1636622716.742135
+    assert pcd.shape == (27300, 6)
+    assert pcd.dtype.name == "float32"
+
+
+def test_bag_as_struct_array(sample_bag_path, config):
+    pcds = list(vd.read_bag(sample_bag_path, config, "/velodyne_packets", as_pcl_structs=True))
+    assert len(pcds) == 13
+    stamp, pcd, topic = pcds[0]
+    assert topic == "/velodyne_packets"
+    assert stamp.to_sec() == 1636622716.742135
+    assert pcd.shape == (27300,)
+    assert pcd.dtype == pcl_struct_dtype
+
+
+def test_bag_automatic_topic(sample_bag_path, config):
+    pcds = list(vd.read_bag(sample_bag_path, config))
+    assert len(pcds) == 13
+    stamp, pcd, topic = pcds[0]
+    assert topic == "/velodyne_packets"
+    assert stamp.to_sec() == 1636622716.742135
+    assert pcd.shape == (27300, 6)
+
+
+def test_bag_ros_time(sample_bag_path, config):
+    pcds = list(vd.read_bag(sample_bag_path, config, use_header_time=False))
+    assert len(pcds) == 13
+    stamp, pcd, topic = pcds[0]
+    assert topic == "/velodyne_packets"
+    assert stamp.to_sec() == 1636622716.7427142
+    assert pcd.shape == (27300, 6)
