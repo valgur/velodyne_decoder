cefe0c5d6cae7a93793efddb7bebee31427aab3d
Florian Piewak <florianpiewak@gmx.de>
2022-12-02 15:45:54 +0100

    insert reading of rosbag from ROS1 while system ROS is ROS2

--- velodyne_decoder/__init__.py
+++ velodyne_decoder/__init__.py
@@ -126,6 +126,119 @@ def read_bag(bag_file, config, topics=None, as_pcl_structs=False, use_header_tim
             bag.close()
 
 
+def read_bag_ros1(
+    bag_file,
+    config,
+    topics,
+    as_pcl_structs=False,
+    use_header_time=True,
+    return_frame_id=False,
+    time_range=(None, None),
+):
+    """Decodes and yields all point clouds stored in a ROS bag file based on the rosbags package.
+    This will be possible even if the system ros version is ros2.
+
+    `model` parameter must be set in the provided config.
+    Parameters
+    ----------
+    bag_file : path or file handle
+    config : Config
+    topics : str or list of str
+    as_pcl_structs : bool
+        If False, the returned NumPy arrays will be a contiguous array of floats (default).
+        If True, the returned NumPy arrays will contain PCL-compatible structs with dtype
+        {'names': ['x', 'y', 'z', 'intensity', 'ring', 'time'],
+         'formats': ['<f4', '<f4', '<f4', '<f4', '<u2', '<f4'],
+         'offsets': [0, 4, 8, 16, 20, 24], 'itemsize': 32}
+    use_header_time : bool
+        If True, scan_msg.header.stamp will be used for the time stamps.
+        If False, the message arrival time will be used.
+    return_frame_id : bool
+        If True, includes the frame_id of the messages in the returned tuple. Defaults to False.
+    time_range : int, int
+        Optionally only return scans from given time range.
+    Yields
+    ------
+    timestamp: int
+    point_cloud : numpy.ndarray
+    topic : str
+    """
+    import types
+    from rosbags.rosbag1 import Reader
+    from rosbags.typesys import register_types, get_types_from_msg
+    from rosbags.serde import deserialize_ros1
+    decoder = ScanDecoder(config)
+    if isinstance(bag_file, Reader):
+        bag = bag_file
+    else:
+        bag = Reader(str(bag_file))
+        bag.open()
+    try:
+        connections = [
+            c for c in bag.connections if c.msgtype == "velodyne_msgs/msg/VelodyneScan"
+        ]
+        if topics is not None:
+            if isinstance(topics, str):
+                connections = [c for c in connections if c.topic == topics]
+            elif isinstance(topics, list):
+                connections = [c for c in connections if c.topic in topics]
+            else:
+                raise AttributeError(
+                    "The type of topics ({}) is not supported!".format(type(topics))
+                )
+        # register custom type
+        if len(connections) > 0:
+            register_types(
+                get_types_from_msg(connections[0].msgdef, connections[0].msgtype)
+            )
+        start_time, end_time = time_range
+        if start_time is not None and not isinstance(start_time, int):
+            raise AttributeError("Can't handle non int start_time (has to be in ns)!")
+        if end_time is not None and not isinstance(end_time, int):
+            raise AttributeError("Can't handle non int end_time (has to be in ns)!")
+        Result = namedtuple("ResultTuple", ("stamp", "points", "topic"))
+        ResultWithFrameId = namedtuple(
+            "ResultTuple", ("stamp", "points", "topic", "frame_id")
+        )
+        for connection, ros_timestamp, rawdata in bag.messages(
+            connections=connections, start=start_time, stop=end_time
+        ):
+            scan_msg = deserialize_ros1(rawdata, connection.msgtype)
+            # create workaround to add required methods
+            def to_sec(self):
+                return float(self.sec) + (float(self.nanosec) / 1_000_000_000)
+            def to_nsec(self):
+                return int(self.sec) * 1_000_000_000 + self.nanosec
+            scan_msg.header.stamp.to_sec = types.MethodType(
+                to_sec, scan_msg.header.stamp
+            )
+            scan_msg.header.stamp.to_nsec = types.MethodType(
+                to_nsec, scan_msg.header.stamp
+            )
+            for i in range(len(scan_msg.packets)):
+                scan_msg.packets[i].stamp.to_sec = types.MethodType(
+                    to_sec, scan_msg.packets[i].stamp
+                )
+                scan_msg.packets[i].stamp.to_nsec = types.MethodType(
+                    to_nsec, scan_msg.packets[i].stamp
+                )
+            # parse data
+            if is_py2:
+                for packet in scan_msg.packets:
+                    packet.data = bytearray(packet.data)
+            stamp = scan_msg.header.stamp if use_header_time else ros_timestamp
+            points = decoder.decode_message(scan_msg, as_pcl_structs)
+            if return_frame_id:
+                yield ResultWithFrameId(
+                    stamp, points, connection.topic, scan_msg.header.frame_id
+                )
+            else:
+                yield Result(stamp, points, connection.topic)
+    finally:
+        if not isinstance(bag_file, Reader):
+            bag.close()
+
+
 @contextmanager
 def _fopen(filein, *args, **kwargs):
     if isinstance(filein, str):  # filename
