765f515501e59f78ef0ee9b8ce92a6e15c80fcb1
Martin Valgur <martin.valgur@gmail.com>
2023-01-26 14:11:42 +0200

    Add sample PCAP files for tests to GitHub
    Originally under Git LFS, but moved to GitHub instead:
    https://github.com/valgur/velodyne_decoder/releases/download/v1.0.1/GT_test_data.zip

---
+++ test/data/mock_vls128_dual.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Convert a single-return VLS-128 pcap file into a mock dual-return VLS-128 pcap file.
+All returns are both strongest and last.
+"""
+import struct
+from pathlib import Path
+
+import dpkt
+from tqdm.auto import tqdm
+
+pcap_path = Path(__file__).parent / "VLS-128_single/VLS-128_single.pcap"
+output_path = Path(__file__).parent / "VLS-128_dual/VLS-128_dual.pcap"
+
+PACKET_SIZE = 1206
+
+FIRING_DURATION = (8 / 3) * 20 * 1e-6
+
+with pcap_path.open("rb") as fin, output_path.open("wb") as fout:
+    pcap_out = dpkt.pcap.Writer(fout)
+    dummy_block = bytearray(100)
+    packet_count = sum(1 for _ in dpkt.pcap.Reader(fin))
+    fin.seek(0)
+    for pcap_stamp, buf in tqdm(dpkt.pcap.Reader(fin), total=packet_count):
+        eth = dpkt.ethernet.Ethernet(buf)
+        try:
+            data = dpkt.ethernet.Ethernet(buf)
+        except dpkt.dpkt.UnpackError:
+            continue
+        while hasattr(data, "data"):
+            data = data.data
+        if len(data) != PACKET_SIZE:
+            pcap_out.writepkt(buf, stamp)
+            continue
+        # Parse
+        blocks = [data[i * 100 : (i + 1) * 100] for i in range(12)]
+        stamp, dual_return_mode, model_id = struct.unpack("<LBB", data[-6:])
+        # Convert three sequences in a single-return packet into three dual-return packets
+        for seq_idx in range(3):
+            new_blocks = []
+            for block_idx in range(4):
+                new_blocks.append(blocks[seq_idx * 4 + block_idx])
+                new_blocks.append(blocks[seq_idx * 4 + block_idx])
+            dummy_block[2:4] = new_blocks[0][2:4]
+            for i in range(4):
+                new_blocks.append(dummy_block)
+            new_stamp = stamp + int(round(FIRING_DURATION * 1e6 * seq_idx))
+            new_buf = b"".join(new_blocks) + struct.pack("<LBB", new_stamp, 0x39, model_id)
+
+            # Re-frame the data and write to PCAP
+            ip = eth.data
+            udp = ip.data
+            udp.data = new_buf
+            udp.ulen = len(udp)
+            ip.data = udp
+            ip.len = len(ip)
+            eth = dpkt.ethernet.Ethernet(data=ip)
+            pcap_out.writepkt(eth, pcap_stamp + FIRING_DURATION * seq_idx)
