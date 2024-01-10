0533359372e2e87448ba5d723fba647e63f5c621
Martin Valgur <martin.valgur@gmail.com>
2024-01-10 18:40:11 +0200

    Update conanfile.py
    Give it a structure closer to a conventional CCI recipe.
    Nanobind should be provided by `scikit-build-core` instead.

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 import os
 
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import copy
 
 required_conan_version = ">=1.52.0"
@@ -9,35 +9,44 @@ required_conan_version = ">=1.52.0"
 
 class VelodyneDecoderConan(ConanFile):
     name = "velodyne_decoder"
-    version = "3.0.0"
-    package_type = "library"
-
     description = "Decoder for raw Velodyne packet data"
-    url = "https://github.com/valgur/velodyne_decoder"
+    version = "3.0.0-pre"
     license = "BSD-3-Clause"
+    url = "https://github.com/valgur/velodyne_decoder"
+
+    package_type = "library"
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "with_python": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "with_python": True,
     }
-    generators = "CMakeDeps", "CMakeToolchain"
 
     exports_sources = ["include/*", "src/*", "test/*", "docs/*", "cmake/*", "CMakeLists.txt"]
 
-    def requirements(self):
-        self.requires("yaml-cpp/0.7.0", headers=True, libs=True)
-        self.requires("ms-gsl/4.0.0", transitive_headers=True, transitive_libs=True)
-        if self.options.with_python:
-            self.requires("pybind11/2.10.1", headers=True, libs=True)
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self)
+
+    def requirements(self):
+        self.requires("yaml-cpp/0.8.0")
+        self.requires("ms-gsl/4.0.0", transitive_headers=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -45,14 +54,13 @@ class VelodyneDecoderConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(
-            self,
-            pattern="*LICENSE",
-            src=self.source_folder,
-            dst=os.path.join(self.package_folder, "licenses"),
-        )
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = [self.name]
+        self.cpp_info.set_property("cmake_file_name", "velodyne_decoder")
+        self.cpp_info.set_property("cmake_target_name", "velodyne_decoder::velodyne_decoder")
+        self.cpp_info.set_property("pkg_config_name", "velodyne_decoder")
+
+        self.cpp_info.libs = ["velodyne_decoder"]
